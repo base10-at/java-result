b262eebbe5757c05b0be1336065bd98b3c7e8d7f
mb <mb@base10.at>
2025-04-11 16:20:17 +0200

    improve testing

--- src/test/java/at/base10/result/ResultTest.java
+++ src/test/java/at/base10/result/ResultTest.java
@@ -114,49 +114,6 @@ class ResultTest {
     }
 
     @Test
-    void test_bind() {
-        var result = Result.<Integer, Integer>success(42).then(bind(x -> success(x + "1")));
-        assertSuccessEquals("421", result);
-    }
-
-    @Test
-    void test_bind_if_Failure() {
-        var result = failure(42).then(bind(x -> success(x + "1")));
-        assertFailureEquals(42, result);
-    }
-
-    @Test
-    void test_bind_either() {
-
-        assertSuccessEquals(43, Result.<Integer, Integer>success(42).then(
-                bindEither(
-                        x -> success(x + 1),
-                        x -> failure(x - 1)
-                )));
-        assertFailureEquals(41, Result.<Integer, Integer>failure(42).then(
-                bindEither(
-                        x -> success(x + 1),
-                        x -> failure(x - 1)
-                )));
-    }
-
-    @Test
-    void test_bindFailure() {
-        Result<String, Void> result = Result.<String, Integer>failure(42).then(bindFailure(x -> success(x + "1", Void.class)));
-        assertSuccessEquals("421", result);
-    }
-
-    @Test
-    void test_bindFailure2() {
-        var result = Result.<String, Integer>failure(42)
-                .then(bindFailure(x -> success(x + "1")))
-                .then(bind(x -> success(x + "1")))
-                .then(bind(x -> success(x + "1")));
-
-        assertSuccessEquals("42111", result);
-    }
-
-    @Test
     void test_recover_failure() {
         Result<String, Void> result = Result.failure(4, String.class)
                 .recover(x -> x + "2");
@@ -182,25 +139,6 @@ class ResultTest {
         Result<String, Void> result = Result.success("42", Integer.class)
                 .then(recover(x -> x + "1"));
         assertSuccessEquals("42", result);
-    }
-
-    @Test
-    void test_bindFailure3() {
-
-        var result = Result.failure(42, String.class)
-                .then(bindFailure(x -> failure(x + "1")))
-                .then(bindFailure(x -> failure(x + "1")))
-                .then(bind(x -> failure(x.trim() + 1, Void.class)))
-                .then(bind(x -> success(1)));
-
-        assertFailureEquals("4211", result);
-    }
-
-    @Test
-    void test_bindFailure_if_success() {
-        var input = Result.<Integer, Integer>success(42);
-        var result = input.then(bindFailure(x -> success(x + 1)));
-        assertSuccessEquals(42, result);
     }
 
     @Test
